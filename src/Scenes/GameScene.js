63287d3a0b0d75564b597ba5fb5e7a1595ffe4b3
Alexandru <rammazzoti2000@gmail.com>
2020-07-28 00:46:30 +0200

    Add collision and overlap fuctions to GameScene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -19,11 +19,18 @@ export default class GameScene extends Phaser.Scene {
     button.setOrigin(0.5, 0.5);
 
     this.add.sprite(300, 100, 'button1');
-    this.add.image(300, 300, 'items', 0);
-    this.physics.add.image(500, 100, 'button1');
+
+    this.chest = this.physics.add.image(300, 300, 'items', 0);
+
+    this.wall = this.physics.add.image(500, 100, 'button1');
+    this.wall.setImmovable();
 
     this.player = this.physics.add.image(32, 32, 'characters', 0);
     this.player.setScale(2);
+    this.player.body.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(this.player, this.chest, () => { console.log('overlap'); });
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
