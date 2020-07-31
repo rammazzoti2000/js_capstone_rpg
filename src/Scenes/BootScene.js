10d356e6b981c6d5c59aa1b83a373949caab2970
Alexandru <rammazzoti2000@gmail.com>
2020-07-31 15:40:35 +0200

    Netlify deployment

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -1,11 +1,13 @@
 import Phaser from 'phaser';
-import mapJSON from '../../assets/level/large_level1.json';
+// import mapJSON from '../../assets/level/large_level1.json';
 import logoImg from '../../assets/zenva_logo.png';
 import button1Img from '../../assets/images/ui/blue_button01.png';
 import itemsImg from '../../assets/images/items.png';
 import charactersImg from '../../assets/images/characters.png';
 import monstersImg from '../../assets/images/monsters.png';
 import backgroundImg from '../../assets/level/background-extruded.png';
+
+const mapJSON = require('../../assets/level/large_level1.json');
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
